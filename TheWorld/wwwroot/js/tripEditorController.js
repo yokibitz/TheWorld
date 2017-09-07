147a148850a41e79b83413525f288c2d5e26ea9d
yokibitz <yokibitz@hotmail.com>
2017-09-07 22:14:34 +0000

    Angular Routing - complete

--- TheWorld/wwwroot/js/tripEditorController.js
+++ TheWorld/wwwroot/js/tripEditorController.js
@@ -14,7 +14,9 @@
         vm.isBusy = true;
         vm.newStop = {};
 
-        $http.get("/api/trips/" + vm.tripName + "/stops")
+        var url = "/api/trips/" + vm.tripName + "/stops";
+
+        $http.get(url)
             .then(function (response) {
                 // success
                 angular.copy(response.data, vm.stops);
@@ -26,6 +28,24 @@
             .finally(function () {
                 vm.isBusy = false;
             });
+
+        vm.addStop = function () {
+            vm.isBusy = true;
+
+            $http.post(url, vm.newStop)
+                .then(function (response) {
+                    // success
+                    vm.stops.push(response.data);
+                    _showMap(vm.stops);
+                    vm.newStop = {};
+                }, function (err) {
+                    // failure
+                    vm.errorMessage = "Failed to add new stop";
+                })
+                .finally(function () {
+                    vm.isBusy = false;
+                });
+        }
     }
 
     function _showMap(stops) {
